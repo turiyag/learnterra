5b721f8e54423daa0ef09ba38082bfcf52d7f6cb
Mitchell Ludwig <mitchell.ludwig@gmail.com>
2019-03-24 12:30:16 -0600

    Adding setup and reqs

---
+++ ops/setup.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+yum install git python3 -y
+
+git clone https://github.com/maludwig/bashrc /tmp/bashrc
+/tmp/bashrc/install
+
+git clone https://github.com/turiyag/learnterra.git /app
